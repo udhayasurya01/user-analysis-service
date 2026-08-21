b35863cdd1e264847eb5fc9d714493997f9846d4
udhayasurya01 <udhaya.surya@enochshipping.com>
2026-08-21 12:16:19 +0530

    Initial commit

--- src/main/java/com/enoch/analysis/AnalysisApplication.java
+++ src/main/java/com/enoch/analysis/AnalysisApplication.java
@@ -3,11 +3,11 @@ package com.enoch.analysis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableCaching
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class  AnalysisApplication {
 
     public static void main(String[] args) {
